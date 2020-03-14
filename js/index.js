f76c0a24093de2496cb2c681c42a7cec0133d793
jesuscaleb <almostperfect557@gmail.com>
2020-03-14 11:04:28 -0500

    Primer modelo de la página s4hana

--- js/index.js
+++ js/index.js
@@ -18,6 +18,11 @@ $.validator.addMethod("nameRegex", function(value, element) {
     return this.optional(element) || /^[a-z0-9\ \s]+$/i.test(value);
 }, "Name must contain only letters, number &  space");
 
+$('.download').on('click', function () {
+    var data =  $(this).attr('data-file');
+    SaveToDisk('/files/'+ data + '.png', 'CURSO ' + data.toUpperCase() );
+});
+
 // Forzar descarga de un archivo
 function SaveToDisk(fileURL, fileName) {
     // para navegadores que no son IE
@@ -71,7 +76,7 @@ $(document).ready(function(){
         utilsScript: "/js/utils.js" // just for formatting/placeholders etc
     }
     );
-    var validator = $('#itsystems-form1').validate({
+    var validator = $('#itsystems-form').validate({
         rules:{
             txtNom:{
                 required:true,
@@ -84,6 +89,10 @@ $(document).ready(function(){
                 alphanumeric:true
             },
             txtEmpresa:{
+                required: true,
+                nameRegex:true
+            },
+            txtCargo:{
                 required: true,
                 nameRegex:true
             },
@@ -135,20 +144,20 @@ $(document).ready(function(){
             .append('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
 
             // Variables para el envio
-            var nombre = $('#itsystems-form1').find('input[name="txtNom"]').val();
+            var nombre = $('#itsystems-form').find('input[name="txtNom"]').val();
             var digit1 = iti1.getSelectedCountryData().dialCode;
-            var digit2 = $('#itsystems-form1').find('input[name="txtTelf"]').val();
+            var digit2 = $('#itsystems-form').find('input[name="txtTelf"]').val();
             var telf = digit1 + digit2;
-            var empresa = $('#itsystems-form1').find('input[name="txtEmpresa"]').val();
-            var email = $('#itsystems-form1').find('input[name="txtEmail"]').val();
+            var empresa = $('#itsystems-form').find('input[name="txtEmpresa"]').val();
+            var cargo = $('#itsystems-form').find('input[name="txtCargo"]').val();
+            var email = $('#itsystems-form').find('input[name="txtEmail"]').val();
             var paisSel = iti1.getSelectedCountryData().name;
             var ciudad = countryCity;
             var paisR = countryName;
             // Nombre del temario
-            var temario = "Facturacion Electronica";
 
             $.ajax({
-                url: 'https://cors-anywhere.herokuapp.com/https://docs.google.com/forms/d/e/1FAIpQLSfiXSeGjuy0G8ifDtZiUWsm1j_cgXg7Vm-clmmhKpFNp6bl_Q/formResponse',
+                url: '',
                 type: 'POST',
                 // TODO: Cambiar los valores por el formulario actual 
                 data: {
@@ -158,6 +167,7 @@ $(document).ready(function(){
                     "entry.20737030": nombre,
                     "entry.1412086532": email,
                     "entry.1444273475": telf,
+                    "entry.2054711039": cargo,
                     "entry.1811869299": empresa
                 },
                 success: function(data) {
@@ -172,153 +182,14 @@ $(document).ready(function(){
                     // SaveToDisk("files/"+ temario +".pdf", temario);
                     var src_icon = 'img/icons/exito.png';
                     var mensaje = 'Se envió el mensaje con éxito.';
-                    $('.response').append("<img class='mx-auto w-40' src='"+ src_icon +"'>");
-                    $('.response').append("<h4 class='mx-auto text-center mt-4' style='color: rgb(82,82,82);'>"+mensaje+"</h4>");
+                    $('.response').append("<img class='mx-auto pt-5 w-50' src='"+ src_icon +"'>");
+                    $('.response').append("<h4 class='mx-auto pb-5 text-center mt-4' style='color: rgb(35,38,38);'>"+mensaje+"</h4>");
                     /*$('.response')
                     .append("<p class='mx-auto text-center text-white'>Se descargará el temario del curso.</p>");
                     $('.response')
                     .append("<p class='mx-auto pb-6 text-center text-white'>Si no se descarga el archivo, haga click <a target='_blank' href='files/"+temario+".pdf'>Aquí</a>.</p>");*/
                     // Clear the form
 					validator.resetForm();
-                },
-                error: function (e) {
-                    console.log(e);
-                }            
-            });
-            return false;
-        }
-    });
-
-    var input2 = document.querySelector("#txtTelf2");
-    var iti2 = window.intlTelInput(input2, {
-        onlyCountries: [ "cl", "co", "cr", "cu",  "ec", "sv",
-         "gt", "hn", "mx", "ni", "pa", "py", "pe", "do", "uy", "ve", "bo"],
-        localizedCountries : {
-            "do": "Republica Dominicana", "pe": "Peru",
-            "mx": "Mexico", "pa": "Panama"
-        },
-        separateDialCode: true,
-        initialCountry: "auto",
-        geoIpLookup: function(callback) {
-            $.getJSON('https://freegeoip.app/json/', function() {}, "jsonp").always(function(resp) {
-              var countryCode = resp.country_code;
-              countryCity = resp.city;
-              callback(countryCode);
-            });
-          },
-        utilsScript: "/js/utils.js" // just for formatting/placeholders etc
-    }
-    );
-
-    var validator2 = $('#itsystems-form2').validate({
-        rules:{
-            txtNom2:{
-                required:true,
-                nameRegex:true
-            },
-            txtTelf2:{
-                required:true,
-                digits:true,
-                minlength: 7,
-                alphanumeric:true
-            },
-            txtEmpresa2:{
-                required: true,
-                nameRegex:true
-            },
-            txtEmail2:{
-                required:true,
-                email: true
-            },
-            chkAuto2:{
-                required: true
-            }
-        },
-        messages:{
-            txtNom2:{
-                required:"Ingrese sus nombres completos",
-                nameRegex:"Ingrese numeros o letras"
-            },
-            txtTelf2:{
-                required: "Ingrese su numero telefonico",
-                digits: "Ingrese solo numeros",
-                minlength: "Minimo 7 caracteres",
-                alphanumeric:"Ingrese solo numeros"
-            },
-            txtEmpresa2:{
-                required: "Ingrese el nombre de la empresa en la que pertenece",
-                nameRegex:"Ingrese numeros o letras"
-            },
-            txtEmail2:{
-                required:"Ingrese su Correo electrónico",
-                email:"Correo invalido"
-            },
-            chkAuto2:{
-                required: "Este campo es obligatorio"
-            }
-        },
-        errorPlacement: function( label, element ) {
-            if( element.attr( "name" ) === "audience[]" || element.attr( "name" ) === "event_services[]" ) {
-                element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
-            } else if (element.attr("name") == "txtTelf2"){
-                label.insertBefore(".telf-error2");
-            } else {
-                label.insertBefore( element ); // standard behaviour
-            }
-        }
-        ,
-        submitHandler: function(form) {
-            // Mientras carga el envio se bloquea el boton y luego realiza la animacion de carga.
-            $('.btn-enviar').attr('disabled', 'true');
-            $('.btn-enviar')
-            .append('<span id="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
-
-            // Variables para el envio
-            var nombre = $('#itsystems-form2').find('input[name="txtNom2"]').val();
-            var digit1 = iti2.getSelectedCountryData().dialCode;
-            var digit2 = $('#itsystems-form2').find('input[name="txtTelf2"]').val();
-            var telf = digit1 + digit2;            
-            var empresa = $('#itsystems-form2').find('input[name="txtEmpresa2"]').val();
-            var email = $('#itsystems-form2').find('input[name="txtEmail2"]').val();
-            var paisSel = iti2.getSelectedCountryData().name;
-            var paisR = countryName
-            var ciudad = countryCity;
-            // Nombre del temario
-            var temario = "Facturacion Electronica";
-
-            $.ajax({
-                url: 'https://cors-anywhere.herokuapp.com/https://docs.google.com/forms/d/e/1FAIpQLSfiXSeGjuy0G8ifDtZiUWsm1j_cgXg7Vm-clmmhKpFNp6bl_Q/formResponse',
-                type: 'POST',
-                // TODO: Cambiar los valores por el formulario actual 
-                data: {
-                    "entry.1608581245": paisSel,
-                    "entry.1666539079": paisR,
-                    "entry.1016680850":ciudad,
-                    "entry.20737030": nombre,
-                    "entry.1412086532": email,
-                    "entry.1444273475": telf,
-                    "entry.1811869299": empresa
-                },
-                success: function(data) {
-                    console.log(data);
-                    // Remover la etiqueta loading del botón Enviar
-                    $('#loading').remove();
-                    // Eliminar el bloque completo de ambos formularios
-                    $('.after-post').remove();
-                    // Deshabilitar el atributo disabled del botón Enviar
-                    $('.btn-enviar').attr('disabled', 'false');
-                    // Ejecutar la impresión de PDF 
-                    //SaveToDisk("files/"+ temario +".pdf", temario);
-                    var src_icon = 'img/icons/exito.png';
-                    var mensaje = 'Se envió el mensaje con éxito.';
-                    $('.response').append("<img class='mx-auto w-40' src='"+ src_icon +"'>");
-                    $('.response').append("<h4 class='mx-auto text-white text-center mt-4'>"+mensaje+"</h4>");
-                    /*$('.response')
-                    .append("<p class='mx-auto text-center text-white'>Se descargará el temario del curso.</p>");
-                    $('.response')
-                    .append("<p class='mx-auto pb-6 text-center text-white'>Si no se descarga el archivo, haga click <a target='_blank' href='files/"+temario+".pdf'>Aquí</a>.</p>");*/
-                    // Clear the form
-                    validator2.resetForm();
                 },
                 error: function (e) {
                     console.log(e);
